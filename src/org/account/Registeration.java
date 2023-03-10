6eeee559f073f7968887ddc2918f5959de751156
Bharathi978 <sharathibharathi@gmail.com>
2023-03-10 13:57:42 +0530

    checking for login

---
+++ src/org/account/Registeration.java
@@ -0,0 +1,40 @@
+package org.account;
+
+public class Registeration {
+	
+
+	public static void main(String[] args) {
+		byte my = 28;
+		System.out.println("my age is "+my);
+		String firstname  = "bharathi";
+		System.out.println("emp first name "+firstname);
+		
+		long mn = 9789268944l;
+		System.out.println("mobnumb "+mn );
+	boolean malestatus = true;
+	System.out.println("malestatus "+malestatus);
+	
+	float sal = 1234.56789f;
+	System.out.println("salary is " +sal);
+	
+	double exc = 1234.56789;
+	System.out.println("excess sal "+ exc);
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/org/account/Registration.java
@@ -0,0 +1,30 @@
+package org.account;
+
+public class Registration {
+	public static void main(String[] args) {
+		byte age = 25 ;
+		System.out.println("employee age is " + age);
+		short year = 9879;
+		System.out.println("joing year is " + year);
+		
+		int pincode = 627426;
+		System.out.println("pincode is "+ pincode);
+		long aadhar = 970000347943l;
+		System.out.println("aadhar is "+ aadhar);
+		
+		float decimal = 244.1234f;
+		System.out.println("decimal is"+ decimal);
+		
+		double hard = 24.777765234 ;
+		System.out.println("hard is"+ hard);
+		
+		char grade = 'A';
+		System.out.println("employee grade is "+ grade);
+		
+		String name = "kumar";
+		System.out.println("employee name is "+ name);
+		
+		boolean malestatus = true;
+		System.out.println("emp malestatus is "+ malestatus);
+}
+}
